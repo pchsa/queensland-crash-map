0cccca5b303fde9b5b4aa26e900d1998dfd3591e
pchsa <108382720+pchsa@users.noreply.github.com>
2025-03-29 17:17:06 +1000

    feat: Add location parameters for crashes api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
 import cors from 'cors';
-
+import SQL, { SQLStatement } from 'sql-template-strings';
 
 dotenv.config(); // Load .env variables
 
@@ -10,8 +10,8 @@ const app = express();
 app.use(cors()); 
 const PORT = process.env.PORT || 3000;
 
-// Set up a PostgreSQL connection pool
-const pool = new Pool({
+// Set up a PostgreSQL connection
+const pg = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
@@ -20,31 +20,47 @@ const pool = new Pool({
 });
 
 app.get('/crashes', async (req, res) => {
-  const { startDate, endDate } = req.query;
+  const { startDate, endDate, location } = req.query;
 
-  if (!startDate || !endDate) {
-    res.status(400).json({ error: 'startDate and endDate are required' });
+  // Validate dates
+  if (!startDate || !endDate || typeof startDate !== 'string' || typeof endDate !== 'string') {
+    res.status(400).json({ error: 'startDate and endDate required in YYYY-MM format' });
+    return;
+  }
+
+  // validate time
+  if (!location) {
+    res.status(400).json({ error: 'location is required' });
     return;
   }
 
   try {
-    const result = await pool.query(
-      `
+    const query = SQL`
       SELECT crash_ref_number, crash_severity, crash_year, crash_month, crash_day_of_week, crash_hour, crash_longitude, crash_latitude
       FROM crashes
-      WHERE 
-        DATE_TRUNC('month', TO_DATE(crash_month || ' ' || crash_year, 'Month YYYY')) 
-          BETWEEN DATE_TRUNC('month', TO_DATE($1, 'YYYY-MM')) 
-              AND DATE_TRUNC('month', TO_DATE($2, 'YYYY-MM'))
-        AND loc_post_code = '4301';
-      `,
-      [startDate, endDate]
-    );
+      WHERE
+    `;
+
+    query.append(buildDateFilter(startDate, endDate));
+
+    const locationList: string[] = Array.isArray(location) 
+      ? location.map(loc => String(loc)) 
+      : [String(location)];
+    
+    const locationFilters = buildLocationFilter(locationList);
+
+    query.append(SQL` AND (`);
+    locationFilters.forEach((filter, i) => {
+      if (i > 0) query.append(SQL` OR `);
+      query.append(filter);
+    });
+    query.append(SQL`)`);
+    const result = await pg.query(query);
 
     res.json(result.rows);
   } catch (err) {
     console.error('Database error:', err);
-    
+
     res.status(500).json({ error: 'Something went wrong' });
   }
 });
@@ -52,3 +68,30 @@ app.get('/crashes', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+function buildDateFilter(startDate: string, endDate: string): SQLStatement {
+  return SQL`
+    DATE_TRUNC('month', TO_DATE(crash_month || ' ' || crash_year, 'Month YYYY'))
+    BETWEEN DATE_TRUNC('month', TO_DATE(${startDate}, 'YYYY-MM'))
+    AND DATE_TRUNC('month', TO_DATE(${endDate}, 'YYYY-MM'))
+  `;
+}
+
+function buildLocationFilter(locations: string[]): SQLStatement[] {
+  const filters: SQLStatement[] = [];
+
+  for (const loc of locations) {
+    const [type, value] = loc.split(':');
+    
+    switch (type.toLowerCase()) {
+      case 'suburb':
+        filters.push(SQL`LOWER(Loc_Suburb) = ${value.toLowerCase()}`);
+        break;
+      case 'lga':
+        filters.push(SQL`LOWER(Loc_Local_Government_Area) = ${value.toLowerCase()}`);
+        break;
+    }
+  }
+
+  return filters;
+}
