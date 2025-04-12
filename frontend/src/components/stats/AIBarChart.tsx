eb15654c2acdeb0ccb653145b9b06576c9cebbaa
pchsa <108382720+pchsa@users.noreply.github.com>
2025-04-12 15:44:05 +1000

    feat: handle errors in AIBarChart

--- frontend/src/components/stats/AIBarChart.tsx
+++ frontend/src/components/stats/AIBarChart.tsx
@@ -5,6 +5,7 @@ import { AIChartData, CrashQuery } from "../../types";
 import { useState } from "react";
 import { fetchAIChartData } from "../../api";
 import { useFilterStore } from "../../store";
+import axios from "axios";
 
 function AIBarChart() {
   const { location, dateRange } = useFilterStore();
@@ -16,28 +17,39 @@ function AIBarChart() {
     series: [{ name: "count" }],
     data: data,
   });
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const handleGenerateChart = () => {
+  const handleGenerateChart = async () => {
+    setLoading(true);
+    setError("");
     console.log("Generating chart with prompt:", prompt);
-    fetchAIChartData(getCrashQuery(location, dateRange), prompt).then(
-      (value) => {
-        console.log(value);
-        setChartData(value);
+    try {
+      const data = await fetchAIChartData(
+        getCrashQuery(location, dateRange),
+        prompt
+      );
+      setChartData(data);
+    } catch (err: any) {
+      if (axios.isAxiosError(err) && err.response?.status === 400) {
+        setError(err.response.data.error);
       }
-    );
+    }
+    setLoading(false);
   };
 
   return (
     <Stack>
       <Group>
         <TextInput
+          error={error}
           placeholder="Generate bar chart about..."
           size="sm"
           value={prompt}
           onChange={(event) => setPrompt(event.currentTarget.value)}
         />
         <ActionIcon
-          disabled={prompt.length == 0}
+          disabled={prompt.length == 0 || loading}
           size="input-sm"
           variant="filled"
           onClick={handleGenerateChart}
